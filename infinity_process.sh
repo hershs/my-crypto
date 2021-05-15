37ff77a239e306a532a9f9f9de5d548955d1963f
hershs </IuOwb2gXT>
2021-05-15 10:25:57 +0300

    .

--- infinity_process.sh
+++ infinity_process.sh
@@ -10,5 +10,5 @@ do
 	-P stratums://0x929058839a423aeb69841a79bf9b313c725b267d.${INSTANCE_ID}@us2.ethermine.org:5555  \
 	-P stratums://0x929058839a423aeb69841a79bf9b313c725b267d.${INSTANCE_ID}@eu1.ethermine.org:5555  \
 	-P stratums://0x929058839a423aeb69841a79bf9b313c725b267d.${INSTANCE_ID}@asia1.ethermine.org:5555  \
-	>> eth.log 2>&1
+	>> /home/ubuntu/my_crypto/eth.log 2>&1
 done
